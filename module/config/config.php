bf3d6213589f4fbe80266979e81f264cd6947b73
David Molineus <mail@netzmacht.de>
2014-03-14 09:35:42 +0100

    fix hooks namespace

--- module/config/config.php
+++ module/config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 // Hooks
-$GLOBALS['TL_HOOKS']['initializeSystem']['bootstrap-core']  = array('Netzmacht\Bootstrap\Core\Hooks', 'initializeEnvironment');
-$GLOBALS['TL_HOOKS']['replaceInsertTags']['bootstrap-core'] = array('Netzmacht\Bootstrap\Core\Hooks', 'replaceInsertTags');
+$GLOBALS['TL_HOOKS']['initializeSystem']['bootstrap-core']  = array('Netzmacht\Bootstrap\Core\Contao\Hooks', 'initializeEnvironment');
+$GLOBALS['TL_HOOKS']['replaceInsertTags']['bootstrap-core'] = array('Netzmacht\Bootstrap\Core\Contao\Hooks', 'replaceInsertTags');
 
 // Event subscribers
 $GLOBALS['TL_EVENT_SUBSCRIBERS'][] = 'Netzmacht\Bootstrap\Core\Subscriber\DefaultSubscriber';
