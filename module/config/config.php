4d8070ca1353c9167c1ed17ad6b64def32529efd
David Molineus <mail@netzmacht.de>
2014-03-19 10:02:09 +0100

    add backend style sheets

--- module/config/config.php
+++ module/config/config.php
@@ -4,11 +4,12 @@
 $GLOBALS['TL_HOOKS']['initializeSystem']['bootstrap-core']  = array('Netzmacht\Bootstrap\Core\Contao\Hooks', 'initializeSystem');
 $GLOBALS['TL_HOOKS']['replaceInsertTags']['bootstrap-core'] = array('Netzmacht\Bootstrap\Core\Contao\Hooks', 'replaceInsertTags');
 
+
 // Event subscribers
 $GLOBALS['TL_EVENT_SUBSCRIBERS'][] = 'Netzmacht\Bootstrap\Core\Subscriber\DefaultSubscriber';
 
-// Bootstrap config
 
+// Bootstrap config
 $GLOBALS['BOOTSTRAP']['dropdown'] = array
 (
 	// element which is used as download toggler
@@ -20,3 +21,9 @@ $GLOBALS['BOOTSTRAP']['dropdown'] = array
 		/* 'mod_quicknav' */
 	),
 );
+
+
+// Add
+if(TL_MODE == 'BE') {
+	$GLOBALS['TL_CSS']['bootstrap-core'] = 'system/modules/bootstrap-core/assets/css/backend.css|static';
+}
