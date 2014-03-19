d156ea48511f6aaea26557718e02c4868bbef0df
David Molineus <mail@netzmacht.de>
2014-03-19 09:59:10 +0100

    add page picker

--- src/Contao/DataContainer/Module.php
+++ src/Contao/DataContainer/Module.php
@@ -72,4 +72,26 @@ class Module
 		return \Controller::getTemplateGroup($prefix, $key);
 	}
 
+
+	/**
+	 * @param \DataContainer $dc
+	 *
+	 * @return string
+	 */
+	public function pagePicker(\DataContainer $dc)
+	{
+		return sprintf(
+			' <a href="contao/page.php?do=%s&amp;table=%s&amp;field=%s&amp;value=%s" title="%s" onclick="Backend.getScrollOffset();Backend.openModalSelector({\'width\':765,\'title\':\'%s\',\'url\':this.href,\'id\':\'%s\',\'tag\':\'ctrl_%s\',\'self\':this});return false">%s</a>',
+			\Input::get('do'),
+			$dc->table,
+			$dc->field,
+			str_replace(array('{{link_url::', '}}'), '', $dc->value),
+			specialchars($GLOBALS['TL_LANG']['MSC']['pagepicker']),
+			specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['MOD']['page'][0])),
+			$dc->field,
+			$dc->field . ((\Input::get('act') == 'editAll') ? '_' . $dc->id : ''),
+			\Image::getHtml('pickpage.gif', $GLOBALS['TL_LANG']['MSC']['pagepicker'], 'style="vertical-align:top;cursor:pointer"')
+		);
+	}
+
 } 
