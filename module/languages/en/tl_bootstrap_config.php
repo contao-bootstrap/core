c844bf5a9c7e1e3ead8c425662126a0f9b7c7a1d
David Molineus <mail@netzmacht.de>
2014-10-24 16:20:33 +0200

    update english translation

--- module/languages/en/tl_bootstrap_config.php
+++ module/languages/en/tl_bootstrap_config.php
@@ -6,50 +6,50 @@ $GLOBALS['TL_LANG']['bootstrap_config_type']['dropdown']  = 'Dropdown';
 
 // legends
 $GLOBALS['TL_LANG']['tl_bootstrap_config']['type_legend'] = 'Typ';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['config_legend'] = 'Konfiguration';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['published_legend'] = 'Veröffentlichung';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['description_legend'] = 'Beschreibung';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['config_legend'] = 'Configuration';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['published_legend'] = 'Publishing';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['description_legend'] = 'Description';
 
 // buttons
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['new'][0] = 'Konfiguration anlegen';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['new'][1] = 'Neue Konfiguration anlegen';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['edit'][0] = 'Konfiguration bearbeiten';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['edit'][1] = 'Konfiguration "ID %s" bearbeiten';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['show'][0] = 'Konfiguration anzeigen';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['show'][1] = 'Konfiguration "ID %s" anzeigen';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['delete'][0] = 'Konfiguration löschen';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['delete'][1] = 'Konfiguration "ID %s" löschen';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['toggle'][0] = 'Konfiguration anwenden/deaktivieren';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['toggle'][1] = 'Konfiguration "ID %s" anwenden/deaktivieren';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['override'][0] = 'Konfiguration überschreiben';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['override'][1] = 'Konfiguration überschreiben';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['description'][0] = 'Beschreibung';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['description'][1] = 'Geben Sie eine Beschreibung ein. Diese wird nur im Backend angezeigt.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['new'][0] = 'New configuration';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['new'][1] = 'Create new configuration';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['edit'][0] = 'Edit configuration';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['edit'][1] = 'Edit configuration "ID %s"';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['show'][0] = 'Show configuration';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['show'][1] = 'Show configuration "ID %s"';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['delete'][0] = 'Delete configuration';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['delete'][1] = 'Delete configuration "ID %s"';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['toggle'][0] = 'Enable/disable configuration';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['toggle'][1] = 'Enable/disable configuration "ID %s';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['override'][0] = 'Override configuration';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['override'][1] = 'Override configuration';
 
 // common field
 $GLOBALS['TL_LANG']['tl_bootstrap_config']['type'][0] = 'Typ';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['type'][1] = 'Wählen sie den Konfigurations-Typ aus';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['type'][1] = 'Please choose a configuration type.';
 $GLOBALS['TL_LANG']['tl_bootstrap_config']['name'][0] = 'Name';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['name'][1] = 'Geben Sie einen eindeutigen Namen für den Konfigurations-Typen ein.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['published'][0] = 'Konfiguration anwenden';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['published'][1] = 'Konfiguration ist veröffentlicht und wird im Theme angewandt.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['name'][1] = 'Please insert a unique name for the configuration type.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['description'][0] = 'Description';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['description'][1] = 'Please insert a description. It\'s only used in the backend';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['published'][0] = 'Enable configuration';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['published'][1] = 'Enable configuration.';
 
 // icons
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_path'][0] = 'Pfad zur PHP-Datei';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_path'][1] = 'Pfad zur PHP-Datei des Icon-Sets.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_path'][0] = 'PHP config file';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_path'][1] = 'Path to the PHP config file of the icon set.';
 $GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_template'][0] = 'Icon Template';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_template'][1] = 'Template zur Generierung des Icons. Erwartet ein %s als Platzhalter.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_files'][0] = 'Stylesheet-Dateien';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_files'][1] = 'Wählen Sie eine oder mehrere Stylesheet-Dateien aus.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_source'][0] = 'Stylesheet-Quelle';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_source'][1] = 'Wählen Sie eine Quelle für die Stylesheets aus.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_paths'][0] = 'Stylesheet-Dateien';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_paths'][1] = 'Geben Sie pro Zeile eine Stylesheet-Datei als relativer Pfad zum Wurzelverzeichnis oder als Url an.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_template'][1] = 'The template will be used for generating the icon as html. Expects an "%s" placeholder for the icon name.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_files'][0] = 'Stylesheet files';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_files'][1] = 'Please choose the icon stylesheet files.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_source'][0] = 'Stylesheet source';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_source'][1] = 'Please choose how to define the stylesheet source.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_paths'][0] = 'Stylesheet paths';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['icons_paths'][1] = 'Please insert one or more stylesheet paths as relative url to your ROOT directory. External urls are allowed as well.';
 
 // dropdown
 $GLOBALS['TL_LANG']['tl_bootstrap_config']['dropdown_toggle'][0] = 'Dropdown-Toggle';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['dropdown_toggle'][1] = 'HTML-Snippet zur Generierung des Toggle-Symbols.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['dropdown_formless'][0] = 'Dropdowns ohne Formularelemente';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['dropdown_formless'][1] = 'Manche Module bringen ein Dropdown mit. Entscheiden Sie, ob das Select-Element als Dropdown gerendert werden soll.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['dropdown_toggle'][1] = 'HTML-snippet used for the dropdown toggle symbol.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['dropdown_formless'][0] = 'Formless dropdown';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['dropdown_formless'][1] = 'Some module uses a dropdown. You can activate a formless dropdown rendering.';
 $GLOBALS['TL_LANG']['tl_bootstrap_config']['dropdown_formless_template'][0] = 'Template';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['dropdown_formless_template'][1] = 'Wählen Sie ein Template aus, indem die Einstellung wirksam werden.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['dropdown_formless_template'][1] = 'Please choose the templates being used for the formless dropdown.';
