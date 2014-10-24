2ec9bdd2799f5f570383161ade406d26e7fe8734
David Molineus <mail@netzmacht.de>
2014-10-24 16:24:55 +0200

    add bootstrap config label

--- module/languages/en/default.php
+++ module/languages/en/default.php
@@ -1,11 +1,11 @@
 <?php
 
-/**
- * content elements
+/*
+ * Eontent elements
  */
 $GLOBALS['TL_LANG']['CTE']['bootstrap'] = 'Bootstrap';
 
-/**
- * errors
+/*
+ * Errors
  */
 $GLOBALS['TL_LANG']['ERR']['wrapperStartNotExists'] = 'The related start-element of "%s" could not be found. Please set it up first.';
---
+++ module/languages/en/tl_theme.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_theme']['bootstrap_config'][0] = 'Bootstrap configuration';
+$GLOBALS['TL_LANG']['tl_theme']['bootstrap_config'][1] = 'Edit Bootstrap configuration';
