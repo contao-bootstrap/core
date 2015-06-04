d5ac19c878a786fe707415bd4b3564c841633ba1
David Molineus <david.molineus@netzmacht.de>
2015-06-04 17:56:51 +0200

    Update language file.

--- module/languages/de/default.php
+++ module/languages/de/default.php
@@ -3,15 +3,9 @@
 /**
  * content elements
  */
-$GLOBALS['TL_LANG']['CTE']['bootstrap'] = 'Bootstrap';
 
-/**
- * errors
- */
+$GLOBALS['TL_LANG']['CTE']['bootstrap']             = 'Bootstrap';
 $GLOBALS['TL_LANG']['ERR']['wrapperStartNotExists'] = 'Das zugehörige Startelement für "%s" wurde nicht gefunden. Bitte legen Sie dieses erst an.';
+$GLOBALS['TL_LANG']['bootstrapConfig']['0']         = 'Bootstrap-Konfiguration';
+$GLOBALS['TL_LANG']['bootstrapConfig']['1']         = 'Bearbeiten Sie die Bootstrap-Konfiguration.';
 
-/*
- * Bootstrap config menu entry
- */
-$GLOBALS['TL_LANG']['bootstrapConfig'][0] = 'Bootstrap-Konfiguration';
-$GLOBALS['TL_LANG']['bootstrapConfig'][1] = 'Die Bootstrap-Konfiguration bearbeiten';
