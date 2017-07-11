65e8492ac498e791ea0752360a44ca64843a12e0
David Molineus <david.molineus@netzmacht.de>
2017-07-11 12:53:35 +0200

    Update copyright year.

--- src/ContaoBootstrapCoreBundle.php
+++ src/ContaoBootstrapCoreBundle.php
@@ -3,7 +3,7 @@
 /**
  * @package    contao-bootstrap
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2016 netzmacht David Molineus. All rights reserved.
+ * @copyright  2016-2017 netzmacht David Molineus. All rights reserved.
  * @filesource
  *
  */
