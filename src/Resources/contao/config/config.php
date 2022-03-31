23ec27f49128737a15d03b204df163c3a432f7f4
David Molineus <david.molineus@netzmacht.de>
2022-03-31 16:39:28 +0200

    Drop stylepicker4ward support

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -20,12 +20,7 @@ $GLOBALS['TL_HOOKS']['getPageLayout'][]         = ['contao_bootstrap.core.listen
 $GLOBALS['TL_HOOKS']['parseTemplate'][]         = ['contao_bootstrap.core.listener.parse_template', 'prepare'];
 $GLOBALS['TL_HOOKS']['parseFrontendTemplate'][] = ['contao_bootstrap.core.listener.parse_template', 'parse'];
 
-if (TL_MODE === 'BE') {
+if (defined('TL_MODE') && TL_MODE === 'BE') {
     // Add backend stylesheet
     $GLOBALS['TL_CSS']['bootstrap-core'] = 'bundles/contaobootstrapcore/css/backend.css|all|static';
-
-    // load stylepicker config
-    if (\Contao\Input::get('key') === 'stylepicker4ward_import') {
-        require TL_ROOT . '/vendor/contao-bootstrap/core/src/Resources/contao/onfig/stylepicker.php';
-    }
 }
