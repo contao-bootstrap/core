ba6818a132d00d2f16b9709effc7e028b544fca1
David Molineus <david.molineus@netzmacht.de>
2023-06-13 16:00:03 +0200

    Provide base twig functions

---
+++ src/View/Twig/TwigExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ContaoBootstrap\Core\View\Twig;
+
+use ContaoBootstrap\Core\Environment;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+final class TwigExtension extends AbstractExtension
+{
+    public function __construct(private readonly Environment $environment)
+    {
+    }
+
+    /** {@inheritDoc} */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('contao_bootstrap_config', fn () => $this->environment->getConfig()),
+            new TwigFunction('contao_bootstrap_enabled', fn () => $this->environment->enabled),
+        ];
+    }
+}
